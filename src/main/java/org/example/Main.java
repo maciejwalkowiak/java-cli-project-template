dbbe21b9d8fe12caec954b623e3123815b486a78
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-12-25 21:19:39 +0100

    add picocli

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,26 +2,37 @@ package org.example;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.concurrent.Callable;
 import java.util.stream.Stream;
 
 import org.apache.commons.io.filefilter.WildcardFileFilter;
+import picocli.CommandLine;
 
-public class Main {
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.err.println("directory to search not set");
-            return;
-        }
+@CommandLine.Command(name = "app")
+public class Main implements Callable<Integer> {
 
+    @CommandLine.Parameters(index = "0", description = "directory to search")
+    private Path path;
+
+    @Override public Integer call() throws Exception {
         WildcardFileFilter filter = new WildcardFileFilter("*.java");
 
-        try (Stream<Path> stream = Files.walk(Paths.get(args[0]))) {
+        try (Stream<Path> stream = Files.walk(path)) {
             stream.filter(path -> filter.accept(path.toFile()))
                     .forEach(System.out::println);
+            return 0;
+        } catch (NoSuchFileException e) {
+            System.err.println("specified directory does not exist");
         } catch (IOException e) {
-            System.err.println(e.getMessage());
+            System.err.println("Ooops, an error happened " + e.getMessage());
         }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
     }
 }
